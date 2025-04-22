13237d9937915a57fc803898b7cc837a165264d1
Volker Rath <volker.rath.dublin@gmail.com>
2025-04-22 08:24:41 +0200

    plots improved, new script for femtic jacs

--- py4mt/scripts/correlation_from_alpha.py
+++ py4mt/scripts/correlation_from_alpha.py
@@ -12,16 +12,35 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-alpha = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+alpha = np.arange(0.1,0.95,0.05 )
 
 L = - 1./np.log(alpha)
 
 directory = "/home/vrath/Publish/Current/"
-plt.plot(alpha, L, '-bo')
-# plt.legend(["prior = 50 Ohmm", "prior = 20 Ohmm", "prior = 10 Ohmm"])
-plt.xlabel("alpha")
-plt.ylabel("correlation length L (index difference)")
+
+plt.figure()
+
+plt.plot(alpha, L, color="green", 
+         marker="o", 
+         linestyle="dashed",
+         linewidth=1, 
+         markersize=7,
+         markeredgecolor="red",
+         markerfacecolor="white"
+         )
+
+
+plt.text(0.3, 6.5, r"$L = -\frac{1}{\ln\alpha}$", fontsize = 24)
+
+plt.xlabel(r"$\alpha$",fontsize=18)
+
+plt.ylabel(r"$L$ (index difference)",fontsize=18)
+
+plt.tick_params(labelsize='x-large')
+
 plt.grid("on")
+
+plt.tight_layout()
 
 plt.savefig(directory + "L_from_alpha.pdf")
 plt.savefig(directory + "L_from_alpha.png")
---
+++ py4mt/scripts/merge_jacfiles.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 12:34:21 2025
+
+@author: vrath
+"""
+
+import os
+import sys
+import binascii
+import struct
+import numpy as np
+import fnmatch
+
+PY4MTX_DATA = os.environ["PY4MTX_DATA"]
+PY4MTX_ROOT = os.environ["PY4MTX_ROOT"]
+
+mypath = [PY4MTX_ROOT+"/py4mt/modules/", PY4MTX_ROOT+"/py4mt/scripts/"]
+for pth in mypath:
+    if pth not in sys.path:
+        sys.path.insert(0,pth)
+
+#import modules
+import util as utl
+from version import versionstrg
+
+def get_femtic_sorted_jacfiles(files=[], out=True):
+    numbers = []
+    for file in files:
+        numbers.append(int(file[11:]))
+    numbers = sorted(numbers)
+
+    jacfiles = []
+    for ii in numbers:
+        fil = 'sensMatFreq'+str(ii)
+        jacfiles.append(fil)
+
+    if out:
+        print(jacfiles)
+    return jacfiles
+
+datadir = '/home/vrath/work/tmp/inversion_1/'
+freqread=True
+
+
+
+if freqread:
+    filelist = utl.get_filelist(searchstr=['sensMatFreq*'], searchpath=datadir)
+    filelist = get_femtic_sorted_jacfiles(filelist)
+    
+    fcount = -1
+    for file in filelist:
+        fcount = fcount+1
+        with open(datadir+file,'rb') as readfile:
+            tmp = readfile.read(4)
+            numdata=int(struct.unpack('i', tmp)[0])
+            tmp = readfile.read(4)
+            numpara=int(struct.unpack('i', tmp)[0])     
+            jacsiz = numdata*numpara
+            tmpjac = np.zeros((jacsiz,1))
+            print('jacobian is', numdata, 'times', numpara,':',jacsiz)
+            for jj in np.arange(jacsiz):
+                tmp = readfile.read(8)
+                tmpjac[jj,0] = float(struct.unpack('d', tmp)[0])
+                
+        tmpjac = np.reshape(tmpjac, (numpara, numdata))
+        
+    
+        if fcount == 0:
+            jac = tmpjac
+        else:
+            jac = np.hstack((jac, tmpjac))
+              
+    print(np.shape(jac))
+    
+    J = jac.T
+    np.savez_compressed(datadir+'JacobianFull', J = J)
+else:
+    J = np.load(datadir+'JacobianFull.npz')['J']
+
+JTJ = J.T@J
+JJT = J@J.T
+
+
+np.savez_compressed(datadir+'JacobianProducts', JTJ = JTJ, JJT = JJT)
+
+
+with open(datadir+'roughening_matrix.out', 'r') as readfile:
+       tmprough = readfile.readlines()
+       #.append((tmprough,line), axis=1)
+
+print(np.shape(tmprough))
+
