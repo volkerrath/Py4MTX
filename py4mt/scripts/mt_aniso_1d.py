0b1b7dfc6c9d310b7455346e1a20e82cb9167c7e
Volker Rath <volker.rath.dublin@gmail.com>
2025-10-26 19:31:31 +0100

    driver for 1d foreard modelling, now ready for mcmc

---
+++ py4mt/scripts/mt_aniso_1d.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Created on Thu Oct 23 15:19:01 2025
+
+@author: vrath
+'''
+
+from aniso import *
+import util as utl
+from version import versionstrg
+import os
+import sys
+import time
+from datetime import datetime
+import warnings
+import csv
+import inspect
+
+# Import numerical or other specialised modules
+import numpy as np
+
+PY4MTX_DATA = os.environ['PY4MTX_DATA']
+PY4MTX_ROOT = os.environ['PY4MTX_ROOT']
+
+mypath = [PY4MTX_ROOT+'/py4mt/modules/', PY4MTX_ROOT+'/py4mt/scripts/']
+for pth in mypath:
+    if pth not in sys.path:
+        sys.path.insert(0, pth)
+
+from version import versionstrg
+
+import util as utl
+import viz
+import modem 
+from aniso import *
+
+rhoair = 1.e17
+rng = np.random.default_rng()
+nan = np.nan  # float('NaN')
+version, _ = versionstrg()
+fname = inspect.getfile(inspect.currentframe())
+
+titstrng = utl.print_title(version=version, fname=fname, out=False)
+print(titstrng+'\n\n')
+
+WorkDir = PY4MTX_ROOT+'/aniso/'
+ModFile = WorkDir+'ana.dat'
+ResFile = ModFile.replace('.dat', '_summary.dat')
+ImpFile = ModFile.replace('.dat', '_impedance.dat')
+RhoFile = ModFile.replace('.dat', '_rhophas.dat')
+
+Periods = np.logspace(-4., 4., 41)
+
+pi = np.pi
+mu0 = 4e-7 * pi
+nlmax = 1001
+
+
+# Allocate arrays
+h = np.zeros(nlmax)
+rop = np.zeros((nlmax, 3))
+ustr = np.zeros(nlmax)
+udip = np.zeros(nlmax)
+usla = np.zeros(nlmax)
+
+# === Read model parameters ===
+with open(ModFile, 'r') as f:
+    nl = int(f.readline())
+    for layer in range(nl):
+        line = f.readline().split()
+        h[layer] = float(line[0])
+        rop[layer, :] = list(map(float, line[1:4]))
+        ustr[layer] = float(line[4])
+        udip[layer] = float(line[5])
+        usla[layer] = float(line[6])
+
+# === Compute conductivity tensors and effective parameters ===
+sg, al, at, blt = cpanis(rop[:nl], ustr[:nl], udip[:nl], usla[:nl])
+
+# === Write model summary ===
+with open(ResFile, 'w') as f:
+    line = '# Model parameters'
+    f.write(line + "\n")    
+    line = '# layer, thick (km)  res_max, res_min, strike' 
+    f.write(line + "\n")
+    for layer in range(nl):      
+        line =  f'{layer:5d} {h[layer]:12.4f}'
+        pars = '' 
+        for i in range(3):
+            for j in range(3):
+                pars = pars +f'  {sg[layer, i, j]:14.5f}'
+        line = line +pars
+        f.write(line + "\n")
+# === Loop over periods ===
+with open(ImpFile, 'w') as f:
+    line = '#   PERIOD,  Re Zxx,  Im Zxx,  Re Zxy,  Im Zxy ,  Re Zyx,  Im Zyx,  Re Zyy,  Im Zyy'
+    f.write(line + "\n")
+    for per in Periods:
+        z = z1anis(nl, h[:nl], al[:nl], at[:nl],
+                   blt[:nl], np.array([per]))[:, :, 0]
+        z_flat = z.flatten()
+        interlaced = [f"{tmp.real:.5e} {tmp.imag:.5e}" for tmp in z_flat]
+        line = f"{per:.5e} " + "  ".join(interlaced)
+        f.write(line + "\n")
+
+with open(RhoFile, 'w') as f:
+    line = '#   PERIOD,  Rhoa xx,  Phs xx,  Rhoa xy,  Phs xy,  Rhoa yx,  Phs yx,  Rhoa yy,  Phs yy'
+    f.write(line + "\n")
+    for per in Periods:
+        z = z1anis(nl, h[:nl], al[:nl], at[:nl],
+                   blt[:nl], np.array([per]))[:, :, 0]
+        omega = 2.0 * pi / per
+        prev = 1.0 / (omega * mu0)
+        rapp = np.abs(z)**2 * prev
+        papp = np.array(
+            [[180.0 * dphase(z[ii, jj]) / pi for jj in range(2)] for ii in range(2)])
+        line = [f"{per:.5e}"]
+        for i in range(2):
+            for j in range(2):
+                line.append(f"{rapp[i, j]:.5e}")
+                line.append(f"{papp[i, j]:.1f}")
+        f.write("  ".join(line) + "\n")
+
