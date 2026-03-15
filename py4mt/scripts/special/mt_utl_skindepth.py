34f3844c5997580ac3f7723bf76e5e8833847074
Volker Rath <volker.rath.dublin@gmail.com>
2026-03-15 20:30:48 +0100

    km scale

--- py4mt/scripts/special/mt_utl_skindepth.py
+++ py4mt/scripts/special/mt_utl_skindepth.py
@@ -10,13 +10,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-rho = [1.e-4, 1.e-3, 1.e-2, 1.e-1, 1.]
+rho = [1.e4, 1.e3, 1.e2, 1.e1, 1., 0.1]
 per = [1.e-3, 1.e-2, 1.e-1, 1., 10, 100., 1000] # period in s
 
 count = 0
 for r in rho:
     tmp = np.array([500*np.sqrt(r*T) for T in per]) #skin depth
-    
+
     if count==0:
         d = tmp
     else:
@@ -24,10 +24,10 @@ for r in rho:
     count = count+1
 
 
-plt.loglog(per,d.T)
+plt.loglog(per,d.T/1000)
 plt.grid ()
 rlabel = [str(np.log10(r)) for r in rho]
 plt.legend(rlabel, title='log10(rho) in $\Omega$m' )
 plt.xlabel('period in sec')
-plt.ylabel('induction scale/skin depth in m')
+plt.ylabel('induction scale/skin depth in km')
 plt.savefig('skindepth_logrho.png')
